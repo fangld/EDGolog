6c20f66ac9b3ca65ebb01b3f14738487163ff0af
comun_cp <SND\comun_cp>
2014-04-08 10:00:46 +0000

    2014-4-7: *: Start to implement CUDD to denote the precondition and effect of actions and initial states.

--- Planning/Predicate.cs
+++ Planning/Predicate.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Planning;
 
 namespace Planning
 {
-    public class Predicate: VariableContainer
+    public class Predicate
     {
+        #region Fields
+
+        private List<string> _varaiblesType;
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; set; }
+
+        public int Count
+        {
+            get { return _varaiblesType.Count; }
+        }
+
+        public IReadOnlyList<string> ListVariablesType
+        {
+            get { return _varaiblesType; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Predicate()
+        {
+            _varaiblesType = new List<string>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddVariableTypes(string type, int number)
+        {
+            string[] types = new string[number];
+            for (int i = 0; i < number; i++)
+            {
+                types[i] = type;
+            }
+
+            _varaiblesType.AddRange(types);
+        }
+
+        #endregion
     }
 }
