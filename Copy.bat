2e5864f63cbf9f335c1e4287d0bf66903101de84
comun_cp <SND\comun_cp>
2014-04-28 08:51:16 +0000

    2014-4-28: *: Refactoring DomainLoader for Clients.

--- Copy.bat
+++ Copy.bat
@@ -1,3 +1,2 @@
-copy "LanguageRecognition\obj\Debug\Planning*.cs" "Agents\Planning\"
-copy "LanguageRecognition\obj\Debug\Planning*.cs" "ObjectWorlds\Planning\"
+copy "LanguageRecognition\obj\Debug\Planning*.cs" "Planning\"
 copy "LanguageRecognition\obj\Debug\HighLevelProgram*.cs" "Agents\HighLevelPrograms\"
