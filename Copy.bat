ea83ec78235383f396d05f2c2de0af9b11145adf
comun_cp <SND\comun_cp>
2014-07-12 15:26:13 +0000

    2014-7-12 (Daily Commit) *: Move Copy.bat to LanguageRecognition project +: Add Post generating events

--- Copy.bat
+++
@@ -1,2 +0,0 @@
-copy "LanguageRecognition\obj\Debug\Planning*.cs" "Planning\"
-copy "LanguageRecognition\obj\Debug\HighLevelProgram*.cs" "Agents\HighLevelPrograms\"
---
+++ LanguageRecognition/Copy.bat
@@ -0,0 +1,2 @@
+copy "E:\EDGolog\LanguageRecognition\obj\Debug\Planning*.cs" "E:\EDGolog\Planning\"
+copy "E:\EDGolog\LanguageRecognition\obj\Debug\HighLevelProgram*.cs" "E:\EDGolog\Agents\HighLevelPrograms\"
